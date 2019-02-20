859ba6fd5e1495790eea00b542c3976115c7edbd
Atik <rahmanatawur@gmail.com>
2019-02-20 13:47:39 +0600

    creating migration file & configure database

---
+++ backend/database/migrations/2019_02_20_072157_create_post_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+           
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('category_id');
+            //$table->foreign('category_id')->references('id')->on('categories');
+            $table->string('title', 500);
+            $table->longText('description');
+            $table->longText('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
